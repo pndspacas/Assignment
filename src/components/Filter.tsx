68fdd73887e10ec8c468faf9e60097b91b072bda
Pedro <pndspacas@gmail.com>
2023-11-22 17:52:07 -0800

    Added toggle revenue

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import reset from "../assets/reset.png"
 
 
@@ -7,17 +8,30 @@ interface Props {
 }
 
 const Filter: React.FC<Props> = ({ handleSort, handleReset }) => {
+
+    const [isFocused, setIsFocused] = useState(false);
+    const [selected, setSelected] = useState(false);
+
+    const handleFocus = () => {
+        setIsFocused(!isFocused);
+    };
+
+
     return (
         <>
             <div className="filter-container">
-                <button className="filter-btn" onClick={handleSort}>Top 10 Revenue</button>
-                <button className="filter-btn"> Top 10 Revenue per Year</button>
-                <div className='years-container'>
+                <button className="filter-btn"
+                    onClick={handleSort}>Top 10 Revenue</button>
+                {selected ?
+                    <button className="filter-btn-year"> Top 10 Revenue {year}</button>
+                    :
+                    <button className="filter-btn-year" onClick={handleFocus}> Top 10 Revenue per Year</button>}
+                {isFocused && <div className="years-container">
                     <h5>Select Year</h5>
                     {Array.from({ length: 17 }, (_, index) => (
                         <p key={index}>{2016 - index}</p>
                     ))}
-                </div>
+                </div>}
                 <img src={reset} alt="reset" onClick={handleReset} />
             </div >
         </>
